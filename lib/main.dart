b5ad7bec7ebf9a6f903c71d78aae0ba398ef0e4a
Napatphongjanta <voop45622@hotmail.com>
2024-09-05 15:29:26 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_store/app_router.dart';
+import 'package:flutter_store/themes/style.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      initialRoute: AppRouter.welcome,
+      routes: AppRouter.routes,
+    );
+  }
+}
+
